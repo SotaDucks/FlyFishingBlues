902f6b05a990bffbebabe48f6370eb8c055bbb25
ZOZOrdon <zoetj1023@gmail.com>
2025-05-15 10:45:02 +0800

    1
    1

---
+++ Assets/PointerDestory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerDestory : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // 检查进入触发区域的是否是玩家
+        if (other.CompareTag("Player"))
+        {
+            // 销毁自身
+            Destroy(gameObject);
+        }
+    }
+}
